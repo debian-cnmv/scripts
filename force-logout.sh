6d9d27f57d1decf8129d70dc1ae44a89112e6def
Profesor <nicolescu.cnmv@gmail.com>
2026-02-23 10:45:03 +0200

    Force logout: check if user exists

--- force-logout.sh
+++ force-logout.sh
@@ -3,5 +3,9 @@
 if [[ -z $1 ]]; then
     echo "No user specified!"
 else
-    sudo pkill -KILL -u "$1"
+    if id "$1" &>/dev/null; then
+        sudo pkill -KILL -u "$1"
+    else
+        echo "User $1 does not exist!"
+    fi
 fi
